cf270fdc0bfc0a9a70b5dce27a490b68ddf2bcd6
ACE0220 <895047954@qq.com>
2023-06-12 10:52:18 +0800

    feat: add jest introduction

---
+++ test-framwork/jest-demo/README.md
@@ -0,0 +1,3 @@
+# jest 配置 typescript 支持 demo
+
+文档：https://drumv.cn/tests/jest/intro-jest/
---
+++ test-framwork/jest-demo/jest.config.ts
@@ -0,0 +1,23 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  // [...]
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: ['**/tests/*.test.ts'],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default jestConfig;
